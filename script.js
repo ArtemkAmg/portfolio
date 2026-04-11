1b7768e96f2f04d81348c5ff72e2eb0a6b192915
ArtemkAmg <159916581+ArtemkAmg@users.noreply.github.com>
2026-04-11 19:23:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleTheme() {
+  document.body.classList.toggle('light');
+}
+
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+  sections.forEach(sec => {
+    const top = sec.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      sec.classList.add('show');
+    }
+  });
+});
